91c87de33ff96317bed4d1af4e0244c27801adc6
xulihang <xulihanghai@163.com>
2021-08-15 20:55:48 +0800

    add ocr and mask

--- server.py
+++ server.py
@@ -17,6 +17,46 @@ if os.path.exists("use_cuda"):
 else:
     t = Translator()
 
+@route('/ocr_and_mask', method='POST')
+def ocr_and_get_mask():
+    save_path = "./uploaded/"
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    timestamp=str(int(time.time()*1000))
+    
+    upload = request.files.get('upload')   
+    name, ext = os.path.splitext(upload.filename)
+    print(ext.lower())
+    if ext.lower() not in ('.png','.jpg','.jpeg'):
+        return "File extension not allowed."            
+    savedName=timestamp+ext
+    file_path = "{path}/{file}".format(path=save_path, file=savedName)
+    mask_path = "{path}/{file}".format(path=save_path, file=savedName+"-mask.png")
+    if os.path.exists(file_path)==True:
+        os.remove(file_path)
+    upload.save(file_path)
+    
+    img=cv2.imread(file_path)
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img_bbox = np.copy(img)
+    img_bbox = cv2.bilateralFilter(img_bbox, 17, 80, 80)
+    
+    textlines, mask = t.detect(img_rgb)
+    textlines = t.ocr(img, img_bbox,textlines)
+    mask = t.gen_mask(img_rgb, mask, textlines)
+    cv2.imwrite(mask_path, mask)
+    mask = convert_mask(mask_path)
+    png = cv2.imencode('.png',mask)[1]
+    image_code = str(base64.b64encode(png))[2:-1]
+    result = {}
+    boxes = []
+    for line in textlines:
+        boxes.append(textline_as_map(line))
+    result["boxes"]=boxes
+    result["mask"] = image_code
+    os.remove(file_path)
+    return result
+    
 @route('/ocr', method='POST')
 def ocr():
     save_path = "./uploaded/"
@@ -112,6 +152,7 @@ def convert_mask(mask_path):
                 r[y][x] = 255
     img_BGRA = cv2.merge((b, g, r, a))
     cv2.imwrite(mask_path,img_BGRA)
+    return img_BGRA
     
 @route('/gettxtremoved', method='POST')
 def get_txtremoved():
